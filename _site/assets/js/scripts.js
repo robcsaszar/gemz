bf908243decc351f61111b202de498a3881d2f1e
Rob Csaszar <rob.csaszar@gmail.com>
2021-02-04 01:48:21 +0200

    removed gitignore stuff

---
+++ _site/assets/js/scripts.js
@@ -0,0 +1,27 @@
+var body = document.querySelector('body')
+var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
+var menuContainer = document.querySelector('#main-menu-mobile');
+
+menuTrigger.onclick = function() {
+    menuContainer.classList.toggle('open');
+    menuTrigger.classList.toggle('is-active')
+    body.classList.toggle('lock-scroll')
+}
+
+$(document).ready(function() {
+    if (!window.matchMedia)
+        return;
+
+    var current = $('head > link[rel="icon"][media]');
+    $.each(current, function(i, icon) {
+        var match = window.matchMedia(icon.media);
+        function swap() {
+            if (match.matches) {
+                current.remove();
+                current = $(icon).appendTo('head');
+            }
+        }
+        match.addEventListener('change', swap);
+        swap();
+    });
+});
