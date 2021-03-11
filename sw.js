f5387ec0e529dfa6cd4ced0d9eb84746608695d7
Rob Csaszar <rob.csaszar@gmail.com>
2021-03-11 10:24:10 +0200

    delete sw

--- sw.js
+++ sw.js
@@ -18,48 +18,21 @@ var fontFiles = [
     '/fonts/fraunces-v7-latin-ext-500italic.woff2'
 ];
 
-// On install, cache some stuff
-addEventListener('install', function (event) {
-	event.waitUntil(caches.open('core').then(function (cache) {
-		cache.add(new Request('/offline.html'));
-		return;
-	}));
-});
+self.addEventListener('install', function(e) {
+    self.skipWaiting();
+  });
 
-
-// listen for requests
-addEventListener('fetch', function (event) {
-
-	// Get the request
-	var request = event.request;
-
-	// Bug fix
-	// https://stackoverflow.com/a/49719964
-	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
-	// HTML files
-	// Network-first
-	if (request.headers.get('Accept').includes('text/html')) {
-		event.respondWith(
-			fetch(request).then(function (response) {
-
-				// Save the response to cache
-				if (response.type !== 'opaque') {
-					var copy = response.clone();
-					event.waitUntil(caches.open('pages').then(function (cache) {
-						return cache.put(request, copy);
-					}));
-				}
-
-				// Then return it
-				return response;
-
-			}).catch(function (error) {
-				return caches.match(request).then(function (response) {
-					return response || caches.match('/offline.html');
-				});
-			})
-		);
-	}
-
-});
+  self.addEventListener('activate', function(e) {
+    self.registration.unregister()
+      .then(function() {
+        return self.clients.matchAll();
+      })
+      .then(function(clients) {
+        clients.forEach(client => client.navigate(client.url))
+      });
+      caches.keys().then(function(cacheNames) {
+        cacheNames.forEach(function(cacheName) {
+          caches.delete(cacheName);
+        });
+      });
+  });
