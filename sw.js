c704226308807605401844bb3a140c255bb943db
Rob Csaszar <rob.csaszar@gmail.com>
2021-03-11 09:48:25 +0200

    changed version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = '1.0.2';
+var version = '1.0.0';
 var coreID = version + '_core';
 var pageID = version + '_pages';
 var imgID = version + '_img';
@@ -20,67 +20,85 @@ var fontFiles = [
 
 // On install, cache some stuff
 addEventListener('install', function (event) {
-	event.waitUntil(caches.open('core').then(function (cache) {
-		cache.add(new Request('offline.html'));
-		return;
-	}));
+    event.waitUntil(caches.open(coreID).then(function (cache) {
+        cache.add(new Request('offline.html'));
+        cache.add(new Request('/assets/css/fonts.css'));
+        fontFiles.forEach(function(file) {
+            cache.add(new Request(file));
+        });
+        return;
+    }));
 });
-
 
 // listen for requests
 addEventListener('fetch', function (event) {
 
-	// Get the request
-	var request = event.request;
+    // Get the request
+    var request = event.request;
 
-	// Bug fix
-	// https://stackoverflow.com/a/49719964
-	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+    // Bug fix
+    // https://stackoverflow.com/a/49719964
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
 
-	// HTML files
-	// Network-first
-	if (request.headers.get('Accept').includes('text/html')) {
-		event.respondWith(
-			fetch(request).then(function (response) {
+    // HTML files
+    // Network-first
+    if (request.headers.get('Accept').includes('text/html')) {
+        event.respondWith(
+            fetch(request).then(function (response) {
 
-				// Save the response to cache
-				if (response.type !== 'opaque') {
-					var copy = response.clone();
-					event.waitUntil(caches.open('pages').then(function (cache) {
-						return cache.put(request, copy);
-					}));
-				}
+                // Save the response to cache
+                if (response.type !== 'opaque') {
+                    var copy = response.clone();
+                    event.waitUntil(caches.open(pageID).then(function (cache) {
+                        return cache.put(request, copy);
+                    }));
+                }
 
-				// Then return it
-				return response;
+                // Then return it
+                return response;
 
-			}).catch(function (error) {
-				return caches.match(request).then(function (response) {
-					return response || caches.match('offline.html');
-				});
-			})
-		);
-	}
+            }).catch(function (error) {
+                return caches.match(request).then(function (response) {
+                    return response || caches.match('offline.html');
+                });
+            })
+        );
+    }
 
-	// Images & Fonts
-	// Offline-first
-	if (request.headers.get('Accept').includes('image')) {
-		event.respondWith(
-			caches.match(request).then(function (response) {
-				return response || fetch(request).then(function (response) {
+    // Images & Fonts
+    // Offline-first
+    if (request.headers.get('Accept').includes('image') || request.url.includes('inter-v3') || request.url.includes('fraunces-v7') || request.url.includes('/assets/css/fonts.css')) {
+        event.respondWith(
+            caches.match(request).then(function (response) {
+                return response || fetch(request).then(function (response) {
 
-					// Stash a copy of this image in the images cache
-					var copy = response.clone();
-					event.waitUntil(caches.open('images').then(function (cache) {
-						return cache.put(request, copy);
-					}));
+                    // If an image, stash a copy of this image in the images cache
+                    if (request.headers.get('Accept').includes('image')) {
+                        var copy = response.clone();
+                        event.waitUntil(caches.open(imgID).then(function (cache) {
+                            return cache.put(request, copy);
+                        }));
+                    }
 
-					// Return the requested file
-					return response;
+                    // Return the requested file
+                    return response;
 
-				});
-			})
-		);
-	}
+                });
+            })
+        );
+    }
 
+});
+
+// On version update, remove old cached files
+self.addEventListener('activate', function (event) {
+	event.waitUntil(caches.keys().then(function (keys) {
+		return Promise.all(keys.filter(function (key) {
+			return !cacheIDs.includes(key);
+		}).map(function (key) {
+			return caches.delete(key);
+		}));
+	}).then(function () {
+		return self.clients.claim();
+	}));
 });
