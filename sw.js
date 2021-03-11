b94d524d3cc23a0293a94968ebd6b80f9f456463
Rob Csaszar <rob.csaszar@gmail.com>
2021-03-11 10:27:15 +0200

    re-added sw

--- sw.js
+++ sw.js
@@ -18,21 +18,60 @@ var fontFiles = [
     '/fonts/fraunces-v7-latin-ext-500italic.woff2'
 ];
 
-self.addEventListener('install', function(e) {
-    self.skipWaiting();
-  });
+/**
+ * Check if cached API data is still valid
+ * @param  {Object}  response The response object
+ * @return {Boolean}          If true, cached data is valid
+ */
+ var isValid = function (response) {
+	if (!response) return false;
+	var fetched = response.headers.get('sw-fetched-on');
+	if (fetched && (parseFloat(fetched) + (1000 * 60 * 60 * 2)) > new Date().getTime()) return true;
+	return false;
+};
 
-  self.addEventListener('activate', function(e) {
-    self.registration.unregister()
-      .then(function() {
-        return self.clients.matchAll();
-      })
-      .then(function(clients) {
-        clients.forEach(client => client.navigate(client.url))
-      });
-      caches.keys().then(function(cacheNames) {
-        cacheNames.forEach(function(cacheName) {
-          caches.delete(cacheName);
-        });
-      });
-  });
+// On install, cache some stuff
+addEventListener('install', function (event) {
+	event.waitUntil(caches.open('core').then(function (cache) {
+		cache.add(new Request('/offline.html'));
+		return;
+	}));
+});
+
+
+// listen for requests
+addEventListener('fetch', function (event) {
+
+	// Get the request
+	var request = event.request;
+
+	// Bug fix
+	// https://stackoverflow.com/a/49719964
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+	// HTML files
+	// Network-first
+	if (request.headers.get('Accept').includes('text/html')) {
+		event.respondWith(
+			fetch(request).then(function (response) {
+
+				// Save the response to cache
+				if (response.type !== 'opaque') {
+					var copy = response.clone();
+					event.waitUntil(caches.open('pages').then(function (cache) {
+						return cache.put(request, copy);
+					}));
+				}
+
+				// Then return it
+				return response;
+
+			}).catch(function (error) {
+				return caches.match(request).then(function (response) {
+					return response || caches.match('/offline.html');
+				});
+			})
+		);
+	}
+
+});
