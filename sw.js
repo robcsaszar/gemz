b244069de8a8788919a33401cf57c49c4dffab1d
Rob Csaszar <rob.csaszar@gmail.com>
2021-03-11 19:46:57 +0200

    updated sworker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = '1.0.2';
+var version = '1.0.3';
 var coreID = version + '_core';
 var pageID = version + '_pages';
 var imgID = version + '_img';
@@ -19,49 +19,64 @@ var fontFiles = [
 ];
 
 // On install, cache some stuff
-addEventListener('install', (event) => {
-    self.skipWaiting();
-    event.waitUntil(caches.open(coreID).then((cache) => {
-        cache.add(new Request('/offline.html'));
-        cache.add(new Request('/favicon.ico'));
-        return;
-    }));
+self.addEventListener('install', function (event) {
+	self.skipWaiting();
+	event.waitUntil(caches.open(coreID).then(function (cache) {
+		cache.add(new Request('/offline/'));
+		cache.add(new Request('/favicon.ico'));
+		fontFiles.forEach(function (file) {
+			cache.add(new Request(file));
+		});
+		return cache;
+	}));
+});
+
+// On version update, remove old cached files
+self.addEventListener('activate', function (event) {
+	event.waitUntil(caches.keys().then(function (keys) {
+		return Promise.all(keys.filter(function (key) {
+			return !cacheIDs.includes(key);
+		}).map(function (key) {
+			return caches.delete(key);
+		}));
+	}).then(function () {
+		return self.clients.claim();
+	}));
 });
 
 // listen for requests
-addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function (event) {
 
-    // Get the request
-    var request = event.request;
+	// Get the request
+	var request = event.request;
 
-    // Bug fix
-    // https://stackoverflow.com/a/49719964
-    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
-        return;
+	// Bug fix
+	// https://stackoverflow.com/a/49719964
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
 
-    if (request.method !== 'GET') return;
+	// Ignore non-GET requests
+	if (request.method !== 'GET') return;
 
-    // HTML files
-    // Network-first
-    if (request.headers.get('Accept').includes('text/html')) {
-        event.respondWith(
-            fetch(request).then((response) => {
-
-                // Save the response to cache
-                if (response.type !== 'opaque') {
-                    var copy = response.clone();
-                    event.waitUntil(caches.open(pageID).then((cache) => cache.put(request, copy)));
-                }
-
-                // Then return it
-                return response;
-
-            }).catch(async (error) => {
-                const response = await caches.match(request);
-                return response || caches.match('/offline.html');
-            })
-        );
-    }
+	// HTML files
+	// Network-first
+	if (request.headers.get('Accept').includes('text/html')) {
+		event.respondWith(
+			fetch(request).then(function (response) {
+				if (response.type !== 'opaque') {
+					var copy = response.clone();
+					event.waitUntil(caches.open(pageID).then(function (cache) {
+						return cache.put(request, copy);
+					}));
+				}
+				return response;
+			}).catch(function (error) {
+				return caches.match(request).then(function (response) {
+					return response || caches.match('/offline/');
+				});
+			})
+		);
+		return;
+	}
 
     // Images & Fonts
     // Offline-first
@@ -86,9 +101,4 @@ addEventListener('fetch', (event) => {
 		);
 	}
 
-});
-
-// On version update, remove old cached files
-self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => !cacheIDs.includes(key)).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
 });
