45c3762256641d994b2032b3f439d6068e679f26
Rob Csaszar <rob.csaszar@gmail.com>
2021-03-11 10:16:30 +0200

    simplified ServiceWorker

--- sw.js
+++ sw.js
@@ -6,30 +6,26 @@ var cacheIDs = [coreID, pageID, imgID];
 
 // Font files
 var fontFiles = [
-    'fonts/inter-v3-latin-ext-300.woff',
-    'fonts/inter-v3-latin-ext-300.woff2',
-    'fonts/inter-v3-latin-ext-500.woff',
-    'fonts/inter-v3-latin-ext-500.woff2',
-    'fonts/inter-v3-latin-ext-700.woff',
-    'fonts/inter-v3-latin-ext-700.woff2',
-    'fonts/fraunces-v7-latin-ext-500.woff',
-    'fonts/fraunces-v7-latin-ext-500.woff2',
-    'fonts/fraunces-v7-latin-ext-500italic.woff',
-    'fonts/fraunces-v7-latin-ext-500italic.woff2'
+    '/fonts/inter-v3-latin-ext-300.woff',
+    '/fonts/inter-v3-latin-ext-300.woff2',
+    '/fonts/inter-v3-latin-ext-500.woff',
+    '/fonts/inter-v3-latin-ext-500.woff2',
+    '/fonts/inter-v3-latin-ext-700.woff',
+    '/fonts/inter-v3-latin-ext-700.woff2',
+    '/fonts/fraunces-v7-latin-ext-500.woff',
+    '/fonts/fraunces-v7-latin-ext-500.woff2',
+    '/fonts/fraunces-v7-latin-ext-500italic.woff',
+    '/fonts/fraunces-v7-latin-ext-500italic.woff2'
 ];
 
 // On install, cache some stuff
 addEventListener('install', function (event) {
-    self.skipWaiting();
-    event.waitUntil(caches.open(coreID).then(function (cache) {
-        cache.add(new Request('offline.html'));
-        cache.add(new Request('assets/css/fonts.css'));
-        fontFiles.forEach(function(file) {
-            cache.add(new Request(file));
-        });
-        return;
-    }));
+	event.waitUntil(caches.open('core').then(function (cache) {
+		cache.add(new Request('/offline.html'));
+		return;
+	}));
 });
+
 
 // listen for requests
 addEventListener('fetch', function (event) {
@@ -60,46 +56,10 @@ addEventListener('fetch', function (event) {
 
 			}).catch(function (error) {
 				return caches.match(request).then(function (response) {
-					return response || caches.match('offline.html');
+					return response || caches.match('/offline.html');
 				});
 			})
 		);
 	}
 
-    // Images & Fonts
-    // Offline-first
-    if (request.headers.get('Accept').includes('image') || request.url.includes('inter-v3') || request.url.includes('fraunces-v7') || request.url.includes('assets/css/fonts.css')) {
-		event.respondWith(
-			caches.match(request).then(function (response) {
-				return response || fetch(request).then(function (response) {
-
-					// If an image, stash a copy of this image in the images cache
-					if (request.headers.get('Accept').includes('image')) {
-						var copy = response.clone();
-						event.waitUntil(caches.open('images').then(function (cache) {
-							return cache.put(request, copy);
-						}));
-					}
-
-					// Return the requested file
-					return response;
-
-				});
-			})
-		);
-	}
-
-});
-
-// On version update, remove old cached files
-self.addEventListener('activate', function (event) {
-	event.waitUntil(caches.keys().then(function (keys) {
-		return Promise.all(keys.filter(function (key) {
-			return !cacheIDs.includes(key);
-		}).map(function (key) {
-			return caches.delete(key);
-		}));
-	}).then(function () {
-		return self.clients.claim();
-	}));
 });
