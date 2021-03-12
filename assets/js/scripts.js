999b596ef67915190fcd785c530a2cd94f721d3a
Rob Csaszar <rob.csaszar@gmail.com>
2021-03-12 15:12:40 +0200

    fix mobile menu scroll bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,13 +18,14 @@ $(document).ready(function() {
         $(".container > ul").toggleClass('show-on-mobile');
         e.preventDefault();
     });
-    
+
     if (!window.matchMedia)
         return;
 
     var current = $('head > link[rel="icon"][media]');
     $.each(current, function(i, icon) {
         var match = window.matchMedia(icon.media);
+
         function swap() {
             if (match.matches) {
                 current.remove();
@@ -39,7 +40,7 @@ $(document).ready(function() {
     var counter = 0;
     var displayedShops = 3;
     var numberOfShops = featuredShops.length < displayedShops ? featuredShops.length : displayedShops;
-    
+
 
     while (counter < numberOfShops) {
         var randomIndex;
@@ -56,7 +57,7 @@ $(document).ready(function() {
         randomIndex = Math.floor(Math.random() * featuredShops.length);
 
         if (randomIndexUsed.indexOf(randomIndex) == "-1") {
-            shopUrl = featuredShops[randomIndex]["url"]; 
+            shopUrl = featuredShops[randomIndex]["url"];
             shopName = featuredShops[randomIndex]["name"];
             shopFacebook = featuredShops[randomIndex]["facebook"];
             shopInstagram = featuredShops[randomIndex]["instagram"];
@@ -66,12 +67,12 @@ $(document).ready(function() {
             hasFacebook = shopFacebook.length === 0 ? '' : "<a aria-label='Facebook' class='card-link facebook' href='" + shopFacebook + "'></a>";
 
             hasInstagram = shopInstagram.length === 0 ? '' : "<a aria-label='Instagram' class='card-link instagram' href='" + shopInstagram + "'></a>";
-            
+
             hasWebsite = shopWebsite.length === 0 ? '' : "<a aria-label='Website' class='card-link website' href='" + shopWebsite + "'></a>";
 
-           
 
-            var featuredShop = $('<div class="featured-shop col-12 col-md-4 mb-1"><div class="card-shop"><span class="card-featured"></span><div class="card-social">' + hasFacebook + hasInstagram + hasWebsite +'</div><a class="card-title" href="' + shopUrl + '">' + shopName + '</a><div class="card-overlay"></div><a class="card-url" href="' + shopUrl + '"><img class="shop-image" src="/images/shops' + shopImage + '" alt="' + shopName + '" loading="eager"></a></div></div>');
+
+            var featuredShop = $('<div class="featured-shop col-12 col-md-4 mb-1"><div class="card-shop"><span class="card-featured"></span><div class="card-social">' + hasFacebook + hasInstagram + hasWebsite + '</div><a class="card-title" href="' + shopUrl + '">' + shopName + '</a><div class="card-overlay"></div><a class="card-url" href="' + shopUrl + '"><img class="shop-image" src="/images/shops' + shopImage + '" alt="' + shopName + '" loading="eager"></a></div></div>');
 
             if (counter == (numberOfShops - 1)) {
                 $('#featured-shops').append(featuredShop);
@@ -86,6 +87,13 @@ $(document).ready(function() {
     }
 });
 
+window.onscroll = function(e) {
+    var menuContainer = $('.container > ul');
+    if (menuContainer.hasClass('show-on-mobile')) {
+        menuContainer.show();
+    }
+};
+
 $(window).resize(function() {
     $(".container > ul > li").children("ul").hide();
     $(".container > ul").removeClass('show-on-mobile');
@@ -98,7 +106,7 @@ progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
 progressPath.style.strokeDashoffset = pathLength;
 progressPath.getBoundingClientRect();
 progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-var updateProgress = function () {
+var updateProgress = function() {
     var scroll = $(window).scrollTop();
     var height = $(document).height() - $(window).height();
     var progress = pathLength - (scroll * pathLength / height);
@@ -108,14 +116,14 @@ updateProgress();
 $(window).scroll(updateProgress);
 var offset = 50;
 var duration = 550;
-jQuery(window).on('scroll', function () {
+jQuery(window).on('scroll', function() {
     if (jQuery(this).scrollTop() > offset) {
         jQuery('.progress-wrap').addClass('active-progress');
     } else {
         jQuery('.progress-wrap').removeClass('active-progress');
     }
 });
-jQuery('.progress-wrap').on('click', function (event) {
+jQuery('.progress-wrap').on('click', function(event) {
     event.preventDefault();
     jQuery('html, body').animate({
         scrollTop: 0
